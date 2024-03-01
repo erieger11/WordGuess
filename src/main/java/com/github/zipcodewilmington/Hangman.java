d418987f04da8ceb7acbce97327d5ba1487c2b6f
erieger11 <erieger530@gmail.com>
2024-03-01 10:31:08 -0500

    made the scanner

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,5 +1,7 @@
 package com.github.zipcodewilmington;
 
+import java.util.Scanner;
+import java.sql.SQLOutput;
 
 /**
  * @author xt0fer
@@ -7,5 +9,24 @@ package com.github.zipcodewilmington;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+    public static void main(String[]args){
+        getWord();
+    }
+
+    public static String getWord(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any letter : ");
+        String letter = scan.nextLine();
+        System.out.println("You typed : " + letter);
+        scan.close();
+        return letter;
+    }
+
+
+
+
+
+
+
 }
 //Ethan Rieger
