b4a19abd24e3de18a3528bf1a0e6bddb8cf3eecf
erieger11 <erieger530@gmail.com>
2024-03-04 23:25:10 -0500

    finished

--- src/main/java/com/github/zipcodewilmington/WordGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/WordGuessGame.java
@@ -10,95 +10,90 @@ import java.util.Scanner;
  * @date 5/27/21 11:02 AM
  */
 public class WordGuessGame {
-    boolean toPlay = true;
-    static boolean didYouWin;
+
+    static boolean isPlaying = true;
     static int guessRemaining = 10;
     static int maxGuess = 0;
-    static String choice;
-    static String theUserWord = "________";
+    static String selectedWord;
+    static String theUserWord = "";
     static String theGuess;
+
+
 
 
     public static void main(String[] args) {
         while (true) {
-            if (toPlay()) {
+            if (toPlay().equals("no") ) {
                 endTheGame();
             }
+            selectedWord = theWord();
+            System.out.println(selectedWord);
+            underscoreLine();
             guessRemaining = 10;
-            theUserWord = "________";
-            System.out.println("The word has 8 letters! you have 10 guesses.");
-            System.out.println("________");
+            System.out.println("The word has " +selectedWord.length()+ " letters! you have 10 guesses.");
+            System.out.println(theUserWord);
             while (guessRemaining > maxGuess) {
-                nextGuess();
+                theLetterGuess();
                 theLettersInSecretWord();
                 theCorrectLetters();
-                winGame();
-                if(didYouWin){
-                    break;
-                }
-            }
-            if(!didYouWin){
-                youLose();
+                didYouWin();
+                if(!isPlaying) break;
             }
         }
     }
 
-    public static boolean toPlay() {
+    public static String toPlay() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Do you want to play? Yes/No");
-        choice = scan.next();
-        return Objects.equals(choice, "Yes") || Objects.equals(choice, "No");
+        //todo handle any input thats not yes or no
+        return scan.next().toLowerCase();
+
     }
-    public static String getWord() {
+    public static void theLetterGuess() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter any letter : ");
-        String letter = scan.nextLine();
-        System.out.println("You typed : " + letter);
-        return letter;
+        theGuess = scan.nextLine().toLowerCase();
+        System.out.println("You typed : " + theGuess);
     }
     public static String theWord() {
-        String[] secretWord = new String[]{"Elephant", "Icebergs", "Mountain", "Checkout"};
+        String[] secretWord = new String[]{"elephant", "icebergs", "mountain", "checkout", "tee"};
         return secretWord[new Random().nextInt(secretWord.length)];
     }
     public static void theLettersInSecretWord() {
-        theWord();
         boolean didYouSucceed = false;
-        int i;
-        for (i = 0; i < theWord().length(); i++){
-        char letter = theWord().charAt(i);
-        String stringLetter = String.valueOf(letter);
-        if(Objects.equals(theGuess,stringLetter)) {
-            theUserWord = theUserWord.substring(0, i) + theGuess + theUserWord.substring(i + 1);
-                }
+        for (int i = 0; i < selectedWord.length(); i++){
+            char letter = selectedWord.charAt(i);
+            String stringLetter = String.valueOf(letter);
+            if(Objects.equals(theGuess,stringLetter)) {
+                didYouSucceed = true;
+                theUserWord = theUserWord.substring(0, i) + theGuess + theUserWord.substring(i + 1);
             }
+        }
         if(!didYouSucceed){
-            guessRemaining--;
-            System.out.println("You have one less guess :(");
+            System.out.println("You have " + guessRemaining-- + " guesses remaining!");
         }
     }
 
-    public static void nextGuess() {
-        System.out.println("Guess again!");
-        Scanner sc = new Scanner(System.in);
-        theGuess = sc.next();
-    }
     public static void theCorrectLetters(){
             System.out.println(theUserWord);
         }
-
-    public static void winGame() {
-        if (theUserWord.equals(theWord())) {
-            System.out.println("You won!");
-            didYouWin = true;
-        } else {
-            didYouWin = false;
+    public static void underscoreLine(){
+        for (int i = 0; i < selectedWord.length(); i++) {
+          theUserWord += "_";
         }
     }
-    public static void youLose() {
-        System.out.println("Goodbye!");
+
+    public static void didYouWin() {
+        if (theUserWord.equals(selectedWord)) {
+            System.out.println("You won!");
+
+        } else if(guessRemaining == 0){
+            System.out.println("Goodbye!");
+        }
     }
-    public static void endTheGame(){
-        System.out.println("Exiting the game");
+
+    public static void endTheGame() {
+        System.out.println("Goodbye!");
     }
 }
 
